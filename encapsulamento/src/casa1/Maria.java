1dd47ed8855fb75d4428aaa16069f18074820664
murilopbc <murilopoltronieri3@gmail.com>
2024-05-03 08:29:23 -0300

    encapsulamento

---
+++ encapsulamento/src/casa1/Maria.java
@@ -0,0 +1,8 @@
+package casa1;
+
+public class Maria {
+    private String segredo = "Roubei um carro";
+    String facoDentroDeCasa = "Ronco durante o sono";
+    protected String familiaSabe = "Maria deve no banco";
+    public String todoMundoSabe = "Vou no Bar";
+}
