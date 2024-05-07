a8d97b674fbeaeaca05b61d2d4da21e7714a0fd1
murilopbc <murilopoltronieri3@gmail.com>
2024-05-07 16:23:48 -0300

    last commit

--- Lista 5 - Metodos/Exercicio01/src/Calculo.java
+++ Lista 5 - Metodos/Exercicio01/src/Calculo.java
@@ -8,7 +8,7 @@ public class Calculo {
     }
 
     public static double calcularMediaPonderada(double nota1, double nota2, double peso1, double peso2) {
-        return (nota1 * peso1) + (nota2 * peso2) / (peso1 + peso2);
+        return ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
 
     }
 }
